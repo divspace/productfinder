76457fd58825be764be7165d2ede0ebd12911dc1
Kyle Anderson <kyle@divspace.com>
2015-11-18 11:21:56 -0700

    Use Text::CSV_XS

--- convert.pl
+++ convert.pl
@@ -7,127 +7,150 @@ use Data::Dumper;
 
 use File::Copy;
 use File::Find::Rule;
-use Spreadsheet::XLSX;
+use Text::CSV_XS;
 use Term::ANSIColor qw(:constants);
 
-use constant SRC_IMG_LIST => 'images/list.txt';
-use constant MIS_IMG_LIST => 'images/missing.txt';
-use constant XLS_PRODUCTS => 'products/all.xlsx';
+use constant SRC_CSV_FILE => 'data/afm.csv';
+use constant TRG_TXT_FILE => 'data/afm.txt';
+use constant TRG_MIS_FILE => 'data/missing.txt';
 use constant SRC_IMG_DIR  => '/Volumes/AFM_Artwork';
 use constant TRG_EPS_DIR  => '/Volumes/Raid/MSC Drive/Product Images';
-use constant TRG_IMG_DIR  => '/Volumes/Raid/MSC Drive/Product Images/Web';
+use constant TRG_WEB_DIR  => '/Volumes/Raid/MSC Drive/Product Images/Web';
 
-my $debug = 0;
+my $debug = 1;
 
-if(!-f SRC_IMG_LIST || -z SRC_IMG_LIST) {
+if(!-f TRG_TXT_FILE || -z TRG_TXT_FILE) {
     buildImageList(SRC_IMG_DIR);
 }
 
-open(FH, SRC_IMG_LIST); my @imageList = <FH>; close(FH);
+open(FH, TRG_TXT_FILE); my @imageList = <FH>; close(FH);
 
 if(scalar(@imageList) < 1) {
-    print BOLD, RED, 'No images exist in: ', SRC_IMG_LIST, RESET, "\n";
+    print BOLD, RED, 'No images exist in: ', TRG_TXT_FILE, RESET, "\n";
     exit(1);
 }
 
-my $excelParser = Spreadsheet::XLSX->new(XLS_PRODUCTS, my $converter);
+my $csv = Text::CSV_XS->new({
+    binary => 1,
+    auto_diag => 1,
+    allow_loose_quotes => 1,
+    blank_is_undef => 1,
+    sep_char => '|',
+    escape_char => '\\',
+    eol => $/
+});
 
-foreach my $sheet(@{$excelParser->{Worksheet}}) {
-    foreach my $row(1 .. $sheet->{MaxRow}) {
-        my $upc = $sheet->{Cells}[$row][0]->{Val};
-        my $brand = $sheet->{Cells}[$row][2]->{Val};
+open(my $io, '<', SRC_CSV_FILE);
 
-        if($upc) {
-            my $shortUpc = substr($upc, -5);
-            my $epsImage = TRG_EPS_DIR.'/'.$upc.'.eps';
-            my @upcMatches = grep(/${shortUpc}\.eps$/, @imageList);
+my @rows;
 
-            chomp(@upcMatches);
+while(my $row = $csv->getline($io)) {
+    push(@rows, $row);
+}
 
-            my $upcCount = scalar(@upcMatches);
+$csv->eof or $csv->error_diag();
 
-            if($upcCount == 1) {
-                if(!$debug) {
-                    copyImage($upcMatches[0], $epsImage);
-                    convertImage($epsImage);
-                }
-            } elsif($upcCount > 1) {
-                if($brand) {
-                    my @brandMatches = grep(/${brand}/, @upcMatches);
+close($io);
 
-                    chomp(@brandMatches);
+for my $col(@rows) {
+    my $upc = $col->[9];
+    my $brand = $col->[3];
 
-                    my $brandCount = scalar(@brandMatches);
+    if($upc) {
+        my $shortUpc = $upc;
+        my $epsImage = TRG_EPS_DIR.'/'.$upc.'.eps';
 
-                    if($brandCount == 1) {
+        if(length($upc) > 5) {
+            $shortUpc = substr($upc, -5);
+        }
+
+        my @upcMatches = grep(/${shortUpc}\.eps$/, @imageList);
+
+        chomp(@upcMatches);
+
+        my $upcCount = scalar(@upcMatches);
+
+        if($upcCount == 1) {
+            if(!$debug) {
+                copyImage($upcMatches[0], $epsImage);
+                convertImage($epsImage);
+            }
+        } elsif($upcCount > 1) {
+            if($brand) {
+                my @brandMatches = grep(/${brand}/, @upcMatches);
+
+                chomp(@brandMatches);
+
+                my $brandCount = scalar(@brandMatches);
+
+                if($brandCount == 1) {
+                    if(!$debug) {
+                        copyImage($brandMatches[0], $epsImage);
+                        convertImage($epsImage);
+                    }
+                } elsif($brandCount > 1) {
+                    my @filenames;
+
+                    foreach(@brandMatches) {
+                        m/\/([^\/]+\.eps)/;
+                        push(@filenames, $1);
+                    }
+
+                    if(scalar(arrayUnique(@filenames)) == 1) {
                         if(!$debug) {
                             copyImage($brandMatches[0], $epsImage);
                             convertImage($epsImage);
                         }
-                    } elsif($brandCount > 1) {
-                        my @filenames;
+                    } else {
+                        my $product = $col->[2];
+
+                        if(!$product) {
+                            $product = $brand;
+                        }
+
+                        print BOLD, WHITE, 'The following product has no definitive match:', RESET, "\n\n";
+                        print '  ', BOLD, GREEN, 'UPC      ', RESET, $upc, "\n";
+                        print '  ', BOLD, GREEN, 'BRAND    ', RESET, $brand, "\n";
+                        print '  ', BOLD, GREEN, 'PRODUCT  ', RESET, $product, "\n\n";
+                        print BOLD, WHITE, 'Please select the image you would like to use:', RESET, "\n\n";
+                        print '  ', BOLD, WHITE, '0) ', 'Skip Product', RESET, "\n\n";
+
+                        my $i = 1;
 
                         foreach(@brandMatches) {
                             m/\/([^\/]+\.eps)/;
-                            push(@filenames, $1);
+                            print '  ', BOLD, BLUE, $i.') ', RESET, $1, "\n";
+                            ++$i;
                         }
 
-                        if(scalar(arrayUnique(@filenames)) == 1) {
-                            if(!$debug) {
-                                copyImage($brandMatches[0], $epsImage);
-                                convertImage($epsImage);
-                            }
-                        } else {
-                            my $product = $sheet->{Cells}[$row][6]->{Val};
+                        print "\n".': ';
 
-                            if(!$product) {
-                                $product = $brand;
-                            }
+                        isNumeric: {
+                            my $number = <STDIN>;
 
-                            print BOLD, WHITE, 'The following product has no definitive match:', RESET, "\n\n";
-                            print '  ', BOLD, GREEN, 'UPC      ', RESET, $upc, "\n";
-                            print '  ', BOLD, GREEN, 'BRAND    ', RESET, $brand, "\n";
-                            print '  ', BOLD, GREEN, 'PRODUCT  ', RESET, $product, "\n\n";
-                            print BOLD, WHITE, 'Please select the image you would like to use:', RESET, "\n\n";
-                            print '  ', BOLD, WHITE, '0) ', 'Skip Product', RESET, "\n\n";
+                            chomp($number);
 
-                            my $i = 1;
-
-                            foreach(@brandMatches) {
-                                m/\/([^\/]+\.eps)/;
-                                print '  ', BOLD, BLUE, $i.') ', RESET, $1, "\n";
-                                ++$i;
-                            }
-
-                            print "\n".': ';
-
-                            isNumeric: {
-                                my $number = <STDIN>;
-
-                                chomp($number);
-
-                                if($number !~ /^[0-9]+$/ || $number > $brandCount) {
-                                    print BOLD, RED, "\n", 'Invalid selection. Please try again.', RESET, "\n\n", ': ';
-                                    redo isNumeric;
-                                } else {
-                                    if($number > 0) {
-                                        if(!$debug) {
-                                            copyImage($brandMatches[--$number], $epsImage);
-                                            convertImage($epsImage);
-                                        }
-                                    } else {
-                                        addToMissingList($upc);
+                            if($number !~ /^[0-9]+$/ || $number > $brandCount) {
+                                print BOLD, RED, "\n", 'Invalid selection. Please try again.', RESET, "\n\n", ': ';
+                                redo isNumeric;
+                            } else {
+                                if($number > 0) {
+                                    if(!$debug) {
+                                        copyImage($brandMatches[--$number], $epsImage);
+                                        convertImage($epsImage);
                                     }
+                                } else {
+                                    addToMissingList($upc);
                                 }
                             }
                         }
-                    } else {
-                        addToMissingList($upc);
                     }
+                } else {
+                    addToMissingList($upc);
                 }
-            } else {
-                addToMissingList($upc);
             }
+        } else {
+            addToMissingList($upc);
         }
     }
 }
@@ -159,9 +182,9 @@ sub convertImage {
     shift =~ m/\/(\d+)\.eps$/;
 
     my $sourceImage = TRG_EPS_DIR.'/'.$1.'.eps';
-    my $targetImage = TRG_IMG_DIR.'/'.$1.'.';
+    my $targetImage = TRG_WEB_DIR.'/'.$1.'.';
 
-    mkdir(TRG_IMG_DIR) unless -d TRG_IMG_DIR;
+    mkdir(TRG_WEB_DIR) unless -d TRG_WEB_DIR;
 
     print 'Converting image to JPG...';
     system('convert -density 300 -layers flatten "'.$sourceImage.'" "'.$targetImage.'jpg"');
@@ -175,7 +198,7 @@ sub convertImage {
 sub addToMissingList {
     my $upc = shift;
 
-    open(FH, '>>', MIS_IMG_LIST);
+    open(FH, '>>', TRG_MIS_FILE);
     say FH $upc;
     close(FH);
 }
@@ -190,7 +213,7 @@ sub buildImageList {
         ->name(qr/_\d{5}\.eps$/)
         ->in($dir);
 
-    open(FH, '>>', SRC_IMG_LIST);
+    open(FH, '>>', TRG_TXT_FILE);
 
     foreach my $file(@files) {
         if($file !~ /\/zz?\s?old\s?\/?/i) {
